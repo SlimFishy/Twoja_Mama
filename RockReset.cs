0dc42da09eb85821ee97c817d7eb539413251efa
SlimFishy <36344143+SlimFishy@users.noreply.github.com>
2018-02-11 22:22:49 +0100

    Add files via upload

---
+++ RockReset.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class RockReset : MonoBehaviour
+{
+    public Vector3 Rock;
+    public Vector3 Rock1;
+    private Rigidbody2D rbd2d;
+    void Start()
+    {
+        rbd2d = gameObject.GetComponent<Rigidbody2D>();
+        Rock = GameObject.FindGameObjectWithTag("Rock").transform.position;
+        Rock1 = GameObject.FindGameObjectWithTag("Rock1").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rbd2d.velocity = new Vector2(0, -8);
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Teleport")
+        {
+            
+            transform.position = Rock;
+            
+        }
+        if (col.gameObject.tag == "Teleport1")
+        {
+            transform.position = Rock1;
+            
+        }
+    }
+}
